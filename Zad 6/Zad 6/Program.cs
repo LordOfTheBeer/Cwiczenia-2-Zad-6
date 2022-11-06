d834a00047b6aa4aad71dc67a0f77016a8f4216e
LordOfTheBeer <116457854+LordOfTheBeer@users.noreply.github.com>
2022-11-06 13:54:23 +0100

    Add files via upload

---
+++ Zad 6/Zad 6/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Zad_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int liczba = 987;
+                int s, d, j;
+            s = liczba / 100;
+            
+            liczba = liczba - s * 100;
+            d = liczba / 10;
+            liczba = liczba - d * 10;
+            j = liczba;
+            //Console.WriteLine(s+ " " + d + " " + j);
+            if ((s + d + j) % 3 == 0)
+                Console.WriteLine("podzielna przez 3");
+        }
+    }
+}
